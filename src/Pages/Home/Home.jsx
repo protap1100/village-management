453db4ce7cf9db738998856aef792b5c8e16e8cc
protap1100 <protapb23@gmail.com>
2024-07-05 01:41:48 +0600

    fix:simple navigation fix

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../Components/Shared/Navbar";
+import Footer from "../../Components/Shared/Footer";
 
 const Home = () => {
 
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
