9b686a20e81ada9a01358facf25f901933cc43c1
protap1100 <protapb23@gmail.com>
2024-11-24 23:20:18 +0600

    fix:ui improvement

--- src/Components/Shared/Button.jsx
+++ src/Components/Shared/Button.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Button = ({name,route}) => {
   return (
     <div className="text-center">
-      <button className="p-2 font-bold text-white bg-green-500 hover:bg-green-700 rounded">
+      <button className="p-2 font-bold text-white bg-green-500 hover:bg-green-600 rounded">
         <Link to={route}>See All {name}</Link>
       </button>
     </div>
