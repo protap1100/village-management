75a09b13b1e4578a0fd7512cea66c5a78de2e302
protap1100 <protapb23@gmail.com>
2024-11-28 19:27:49 +0600

    some changes

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -14,30 +14,39 @@ import { GiVillage } from "react-icons/gi";
 const Banner = () => {
   const [text] = useTypewriter({
     words: [
-      "Make Village Beautiful",
-      "Keep Village Clean",
-      "Help Each Other",
-      "Make it Look Clean",
+      "Beautify Our Village",
+      "Maintain Village Cleanliness",
+      "Support and Uplift Each Other",
+      "Strive for a Cleaner Environment",
     ],
+
     loop: true,
     delaySpeed: 1000,
     cursorBlinking: true,
   });
+
   return (
     <div>
       <SectionTitle
-      icon={<GiVillage></GiVillage>}
-        heading={"Welcome To Our Village"}
-        subHeading={"We Believe in Unity"}
+        icon={<GiVillage />}
+        heading={"Discover the Heart of Our Village"}
+        subHeading={"Together, We Create Wonders"}
       ></SectionTitle>
+
       <div className="flex justify-center relative top-20">
-        <div className="text-center text-xl lg:text-2xl font-bold text-green-500 absolute z-10 bg-green-50 w-full mx-2 lg:w-2/4">
+        <div className="text-center text-xl lg:text-2xl font-bold text-green-500 absolute z-10 bg-green-50 w-full mx-2 lg:w-2/4 rounded-lg shadow-lg p-4">
           <h1>
-            Our Goal Is To: {text} <Cursor cursorStyle="_" cursorColor="green" />
+            Our Vision: {text} <Cursor cursorStyle="_" cursorColor="green" />
           </h1>
-          <h1 className="text-xl">We are always working on building our community stronger</h1>
+          <h1 className="text-xl mt-2">
+            Committed to Unity, Growth, and a Brighter Future
+          </h1>
+          <p className="mt-2 text-sm text-gray-700">
+            Join us in making our village a symbol of harmony and progress.
+          </p>
         </div>
       </div>
+
       <Carousel
         className="mt-5 mx-auto cursor-pointer w-full lg:w-5/6 relative"
         autoFocus={true}
@@ -45,28 +54,59 @@ const Banner = () => {
         stopOnHover={true}
         swipeable={true}
         infiniteLoop={true}
-        interval={2000}
+        interval={3000}
+        showThumbs={false}
+        showStatus={false}
+        transitionTime={800}
       >
         <div className="relative">
-          <img className="w-full" src={bannerImage1} />
+          <img
+            className="w-full rounded-lg"
+            src={bannerImage1}
+            alt="Village Banner 1"
+          />
         </div>
         <div className="relative">
-          <img className="w-full" src={bannerImage2} />
+          <img
+            className="w-full rounded-lg"
+            src={bannerImage2}
+            alt="Village Banner 2"
+          />
         </div>
         <div className="relative">
-          <img className="w-full" src={bannerImage3} />
+          <img
+            className="w-full rounded-lg"
+            src={bannerImage3}
+            alt="Village Banner 3"
+          />
         </div>
         <div className="relative">
-          <img className="w-full" src={bannerImage4} />
+          <img
+            className="w-full rounded-lg"
+            src={bannerImage4}
+            alt="Village Banner 4"
+          />
         </div>
         <div className="relative">
-          <img className="w-full" src={bannerImage5} />
+          <img
+            className="w-full rounded-lg"
+            src={bannerImage5}
+            alt="Village Banner 5"
+          />
         </div>
         <div className="relative">
-          <img className="w-full" src={bannerImage6} />
+          <img
+            className="w-full rounded-lg"
+            src={bannerImage6}
+            alt="Village Banner 6"
+          />
         </div>
         <div className="relative">
-          <img className="w-full" src={bannerImage7} />
+          <img
+            className="w-full rounded-lg"
+            src={bannerImage7}
+            alt="Village Banner 7"
+          />
         </div>
       </Carousel>
     </div>
