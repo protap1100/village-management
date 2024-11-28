e897666b476d8a53a02d4b8742c4ffefe88017d2
protap1100 <protapb23@gmail.com>
2024-11-28 19:17:06 +0600

    fix: Loading Spinner fixed

--- src/Others/Loading.jsx
+++ src/Others/Loading.jsx
@@ -1,9 +1,16 @@
 import ReactLoading from "react-loading";
 
-const Loading = ({type}) => {
+const Loading = ({ type }) => {
   return (
     <div className="text-center flex justify-center my-40">
-      <ReactLoading type={type || 'spin'}  color='#8ecae6'  height={100} width={90} />
+      <div className="w-8 h-8 sm:w-20 sm:h-20 md:w-24 md:h-24">
+        <ReactLoading
+          type={type || "spin"}
+          color="#8ecae6"
+          height={"100%"}
+          width={"100%"}
+        />
+      </div>
     </div>
   );
 };
